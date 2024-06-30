c5a2c1f60a7c07f9f401bc528ae82023803011ba
gireesh-busam <busam@uwindsor.ca>
2024-06-30 14:33:21 -0400

    Save form updates_Event creation
    Updated the Post request form while saving the event details for creating the event

--- GreenConscious/MainPage/forms.py
+++ GreenConscious/MainPage/forms.py
@@ -1,19 +1,23 @@
 from django import forms
 
+
 class EventCreationForm(forms.Form):
-    event_name =forms.CharField(required=True,
-                                label='Event Name',
-                                widget=forms.TextInput(attrs={'class':'eventName_field',
-                                                              'placeholder':'Please enter event name'}))
-    event_description =forms.CharField(required=True,
-                                       label='Event Description',
-                                       widget=forms.Textarea(attrs={'class':'form-control',
-                                                                    'placeholder':'Please enter event description'}))
-    event_date =forms.DateField(required=True,
-                                label='Event Date',
-                                widget=forms.SelectDateWidget(attrs={'class':'dateTime_field',}))
-    event_time =forms.TimeField(required=True,
-                                label='Event Time',
-                                widget=forms.TimeInput(format='%H:%M', attrs={'class':'dateTime_field',
-                                                                              'type': 'time',
-                                                                              'placeholder': 'HH:MM'}))
+    event_name = forms.CharField(required=True,
+                                 label='Event Name',
+                                 widget=forms.TextInput(attrs={'class': 'eventName_field',
+                                                               'placeholder': 'Please enter event name'}))
+    event_description = forms.CharField(required=True,
+                                        label='Event Description',
+                                        widget=forms.Textarea(attrs={'class': 'form-control',
+                                                                     'placeholder': 'Please enter event description'}))
+    start_date = forms.DateField(required=True,
+                                 label='Event Start Date',
+                                 widget=forms.SelectDateWidget(attrs={'class': 'dateTime_field'}))
+    end_date = forms.DateField(required=True,
+                               label='Event End Date',
+                               widget=forms.SelectDateWidget(attrs={'class': 'dateTime_field', }))
+    event_time = forms.TimeField(required=True,
+                                 label='Event Time',
+                                 widget=forms.TimeInput(format='%H:%M', attrs={'class': 'dateTime_field',
+                                                                               'type': 'time',
+                                                                               'placeholder': 'HH:MM'}))
